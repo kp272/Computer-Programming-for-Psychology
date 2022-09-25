de1ae731c14cc755a52e0bb4dd646f17adbfab97
Kasti Patel <113375408+kp272@users.noreply.github.com>
2022-09-24 22:54:42 -0600

    Update yourname.py

--- Assignment2/yourname.py
+++ Assignment2/yourname.py
@@ -1,4 +1,4 @@
-### Print exercises 
+#Print exercises 
 
 1)
 print("K")
@@ -7,10 +7,88 @@ print("S")
 print("T")
 print("I")
 
+2)
+print("K")
+print("A")
+print("S")
+print("T")
+print("I")
+K
+A
+S
+T
+I
 
+-------------------------------------------
+-------------------------------------------
+
+#Variable exercises 
+
+1) 
 letter1 = "K"
 letter2 = "A"
 letter3 = "S"
 letter4 = "T"
 letter5 = "I"
+
+2)
+print(letter1)
+print(letter2)
+print(letter3)
+print(letter4)
+print(letter5)
+K
+A
+S
+T
+I
+
+3)
 letterX = "K"
+print(letter1)
+print(letter2)
+print(letter3)
+print(letter4)
+print(letter5)
+print(letterX)
+K
+A
+S
+T
+I
+K
+
+4) NA
+
+5)
+letterX = "X"
+print(letter1)
+print(letter2)
+print(letter3)
+print(letter4)
+print(letter5)
+print(letterX)
+K
+A
+S
+T
+I
+X
+
+6)
+letterX = letter1
+print(letterX)
+print(letter1)
+K
+K
+letter1 = "z"
+letterX = letter1
+print(letterX)
+print(letter1)
+K
+z
+
+-------------------------------------------
+-------------------------------------------
+
+
