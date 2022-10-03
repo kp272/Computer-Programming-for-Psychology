358575784b37127615455ce1d5fccba9cd63a508
Kasti Patel <113375408+kp272@users.noreply.github.com>
2022-10-02 18:42:32 -0600

    Update Assin3.py

--- Assignment3/Assin3.py
+++ Assignment3/Assin3.py
@@ -1,1 +1,29 @@
+### This is my set of answers to Assignment 3:
+
+# Variable operations exercises
+Question 1)
+In:
+    sub_code = "sub"
+    subnr_int = 2
+    subnr_str = "2"
+    print(sub_code + subnr_str)
+    #print(sub_code + subnr_int) 
+Out:
+    sub2
+
+Question 2)
+In:
+    print(sub_code + " " + subnr_str)
+    print(sub_code + " " + subnr_str*3) 
+    print((sub_code + subnr_str)*3)
+    print((sub_code*3)+(subnr_str*3))
+Out: 
+    sub 2
+    sub 222
+    sub2sub2sub2
+    subsubsub222
+
+
+-------------------------------------------
+-------------------------------------------
 
